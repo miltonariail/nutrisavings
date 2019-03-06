bce47ed0dd790903d8e98ce9b8beda71f3cb216e
miltonariail <47499088+miltonariail@users.noreply.github.com>
2019-03-06 12:36:28 -0500

    Create Nutrisavings-Master-Transaction-File-Generator-Script.ps1

---
+++ Nutrisavings-Master-Transaction-File-Generator-Script.ps1
@@ -0,0 +1,45 @@
+﻿
+
+<# 
+   Working As Is ; Need to Dial in the Date Code to make dynamic
+    ...
+#>
+
+Function Download-TrackingFile() {
+$time = (Get-Date).AddDays(-3)
+$Path = "C:\Native Data\Nutrisavings\tracking_files\NS_NativeSun_Tracking_*.txt"
+$DateStamp = ((Get-Date).ToUniversalTime()).ToString("MMddyyy")
+$TestFile = "NS_NativeSun_tracking_" +  $DateStamp + "_010221.txt"
+
+$SFTPusername = "sftpnativesun"
+$encrypted = Get-Content "C:\\Scripts\Data\ScriptsEncrypted_Password_1.txt" | ConvertTo-SecureString
+$Credentials = New-Object System.Management.Automation.PsCredential($SFTPusername, $encrypted)
+$Session = New-SFTPSession -Computername sftp.nutrisavings.com -Credential $Credentials
+
+Get-SFTPfile -SFTPSession $Session -RemoteFile "/Out/$TestFile" -LocalPath "C:\Native Data\Nutrisavings\tracking_files\" -Overwrite
+
+$Session.Disconnect()
+}
+
+Function Update-NutrisavingsMembers() {
+    # Open Excel file
+    $excel = new-object -comobject excel.application
+    $filePath = "C:\Native Data\Control Center\Instacart\Instacart Automation Control File.xlsm"
+    $workbook = $excel.Workbooks.Open($FilePath)
+    $excel.Visible = $true
+    $worksheet = $workbook.worksheets.item(1)
+    $excel.Run("InstacartTax_FileFormatMaster")
+    #$workbook.save()
+    $workbook.close()
+    $excel.quit()
+    Write-Host "Tax File Processed And Converted to XLSX"
+}
+
+
+
+
+Download-TrackingFile
+
+
+
+
